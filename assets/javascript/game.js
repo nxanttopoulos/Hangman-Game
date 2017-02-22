c685c5add23d0091c264f9bb09249cffcb6a4e08
Nicholas Xanttopoulos <nxanttopoulos@gmail.com>
2017-02-22 15:17:58 -0500

    Psychic-Game_Complete

---
+++ assets/javascript/game.js
@@ -0,0 +1,33 @@
+var options = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var win = 0;
+var losses = 0;
+var totalGuesses = 9;
+var guessList = [];
+
+document.onkeyup = function(event) {
+
+	var userGuess = event.key;
+
+	var computerGuess = options[Math.floor(Math.random() * options.length)];
+
+if (userGuess === computerGuess) {
+	alert("Correct!");
+	win++;
+	totalGuesses=9;
+} else {
+	alert("Wrong!");
+	totalGuesses--;
+	guessList.push(userGuess);
+	if (totalGuesses === 0) {
+		alert("You Couldn't Get it!");
+		losses++;
+		totalGuesses=9;
+		guessList=[];
+	}
+}
+
+var game = document.getElementById("game");
+game.innerHTML = "Keep on guessing and see how many you can get right!<br><br>Wins: " 
++ win + "<br><br>Losses: " + losses + "<br><br>Guesses Left:" 
++ totalGuesses + "<br><br>Your Guesses So Far: " + guessList;
+};
